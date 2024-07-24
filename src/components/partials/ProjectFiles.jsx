2dccb7aaf536e3624230cfe82ba61dde1310fb8d
MR-C0DE <80490263+MR-C0DE@users.noreply.github.com>
2024-07-24 01:30:09 -0400

    treddvv

--- src/components/partials/ProjectFiles.jsx
+++ src/components/partials/ProjectFiles.jsx
@@ -1,31 +1,55 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import classNames from "classnames";
 import "../../styles/projectFiles.css";
 
 const ProjectFiles = ({ files }) => {
-    const [index, setIndex] = useState(0);
-    const [select, setSelect] = useState(0);
+  const [index, setIndex] = useState(0);
+  const [select, setSelect] = useState(0);
 
-    const changeFiles = (i) => {
-        setSelect(i);
-        setIndex(i);
-    };
+  const changeFiles = (i) => {
+    setSelect(i);
+    setIndex(i);
+  };
 
-    return (
-        <div className='project-files-container'>
-            {files[index].type === "image" ? (
-                <img className='image-content' src={files[index].url} alt={files[index].name} />
-            ) : (
-                <video className='video-content' src={files[index].url} controls autoPlay loop />
-            )}
+  if (files.length === 0) {
+    return <div>No files available</div>;
+  }
 
-            <div className='file-changer'>
-                {files.map((file, i) => (
-                    <div key={i} className={'btn-change-files ' + (select == i ? "select": "")} onClick={() => changeFiles(i)}></div>
-                ))}
-            </div>
-           
-        </div>
-    );
+  const currentFile = files[index];
+  const isImage = currentFile.type === "image";
+
+  return (
+    <div className="project-files-container">
+      {isImage ? (
+        <img
+          className="image-content"
+          src={currentFile.url}
+          alt={currentFile.name}
+        />
+      ) : (
+        <video
+          className="video-content"
+          src={currentFile.url}
+          controls
+          autoPlay
+          loop
+          aria-label={`Video: ${currentFile.name}`}
+        />
+      )}
+
+      <div className="file-changer">
+        {files.map((file, i) => (
+          <div
+            key={i}
+            className={classNames("btn-change-files", { select: select === i })}
+            onClick={() => changeFiles(i)}
+          >
+            {/* Optional: Add a label or tooltip here if needed */}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default ProjectFiles;
